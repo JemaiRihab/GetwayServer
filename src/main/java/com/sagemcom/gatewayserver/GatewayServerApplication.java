e0cf2da62bde819f738ee99f7125b45d93c77189
Rihabjemai <rihab.dj@pepolls.com>
2023-05-13 17:19:22 +0100

    first commit

---
+++ src/main/java/com/sagemcom/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,22 @@
+package com.sagemcom.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route( r -> r.path("/article/**").uri("http://localhost:9092"))
+                .route( r -> r.path("/stock/**").uri("http://localhost:9090"))
+                .build();
+    }
+}
---
+++ src/test/java/com/sagemcom/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagemcom.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
